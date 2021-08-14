899aef6b8750cf1b1385ce451d0f149a7f72bf20
Khalig Novruzli <khalig.novruzli@gmail.com>
2021-08-14 17:40:15 +0400

    App started

---
+++ screens/Movies.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import {View, Text, StyleSheet} from "react-native";
+
+const Movies = () => {
+    return (
+        <View>
+            <Text>Here goes SW movies</Text>
+        </View>
+    )
+}
+
+export default Movies
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
